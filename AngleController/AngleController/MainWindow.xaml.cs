de1f7654dacb90b90a36bbacc4c241ab391a0884
hh54188 <252438823@qq.com>
2014-07-04 17:51:02 +0800

    update

---
+++ AngleController/AngleController/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace AngleController
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor sensor;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            this.Closing += MainWindow_Closing;
+        }
+
+        void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.sensor != null && this.sensor.IsRunning)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                this.sensor = KinectSensor.KinectSensors[0];
+                if (this.sensor != null && !this.sensor.IsRunning)
+                {
+                    this.sensor.Start();
+                }
+            }
+
+            this.AngleController.Minimum = this.sensor.MinElevationAngle;
+            this.AngleController.Maximum = this.sensor.MaxElevationAngle;
+            this.AngleController.Value = this.sensor.ElevationAngle;
+            this.AngleController.ValueChanged += AngleController_ValueChanged;
+
+            this.AngleLabel.Content = this.sensor.ElevationAngle;
+        }
+
+        void AngleController_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.SetSensorAngle(Convert.ToInt32(e.NewValue));
+        }
+
+        private void SetSensorAngle(int angleValue)
+        {
+            //this.AngleLabel.Content = angleValue;
+            //return;
+
+            if (angleValue > this.sensor.MinElevationAngle && angleValue < this.sensor.MaxElevationAngle)
+            {
+                this.sensor.ElevationAngle = angleValue;
+                this.AngleLabel.Content = angleValue;
+            }
+        }
+    }
+}
---
+++ Camera/Camera/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace Camera
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor sensor;
+        private byte[] pixelData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            this.Closing += MainWindow_Closing;
+        }
+
+        void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.sensor != null && this.sensor.IsRunning)
+            {
+                this.StopSensor();
+            }
+        }
+
+        private void StopSensor()
+        {
+            if (this.sensor != null && this.sensor.IsRunning)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                this.sensor = KinectSensor.KinectSensors[0];
+                this.StartSensor();
+
+                this.sensor.ColorStream.Enable();
+                // Default: this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                this.sensor.ColorFrameReady += sensor_ColorFrameReady;
+            }
+        }
+
+        void sensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame imageFrame = e.OpenColorImageFrame())
+            {
+                if (imageFrame == null)
+                {
+                    return;
+                }
+                else
+                {
+                    this.pixelData = new byte[imageFrame.PixelDataLength];
+                    imageFrame.CopyPixelDataTo(this.pixelData);
+                    int stride = imageFrame.Width * imageFrame.BytesPerPixel;
+
+                    this.VideoControl.Source = BitmapSource.Create(imageFrame.Width, imageFrame.Height, 96, 96, PixelFormats.Bgr32, null, pixelData, stride);
+                }
+            }
+        }
+
+        private void StartSensor()
+        {
+            if (this.sensor != null && !this.sensor.IsRunning)
+            {
+                this.sensor.Start();
+            }
+        }
+    }
+}
