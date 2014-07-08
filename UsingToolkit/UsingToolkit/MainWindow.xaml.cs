8c09a293c9b31965422fe0a5b7a50f004787f406
hh54188 <252438823@qq.com>
2014-07-08 11:43:30 +0800

    update

---
+++ UsingToolkit/UsingToolkit/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Kinect.Toolkit.Controls;
+using Microsoft.Kinect.Toolkit.Interaction; 
+using Coding4Fun.Kinect.Wpf;
+
+namespace UsingToolkit
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor sensor;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            this.Closing += MainWindow_Closing;
+        }
+
+        void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.sensor != null && this.sensor.IsRunning) {
+                this.sensor.Stop();
+            }
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                this.sensor = KinectSensor.KinectSensors[0];
+                if (!this.sensor.IsRunning)
+                {
+                    //this.sensor.ColorFrameReady += sensor_ColorFrameReady;
+                    this.sensor.ColorStream.Enable();
+
+                    this.sensor.DepthFrameReady += sensor_DepthFrameReady;
+                    this.sensor.DepthStream.Enable();
+
+                    this.sensor.Start();
+                }
+            }
+            return;
+        }
+
+        void sensor_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame depthImageFrame = e.OpenDepthImageFrame())
+            {
+                if (depthImageFrame == null)
+                {
+                    return;
+                }
+
+                this.Camera.Source = ImageFrameExtensions.ToBitmapSource(depthImageFrame);
+            }
+            return;
+        }
+
+        void sensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            
+            using (ColorImageFrame colorImageFrame = e.OpenColorImageFrame())
+            {
+                if (colorImageFrame == null)
+                {
+                    return;
+                }
+
+                this.Camera.Source = ImageFrameExtensions.ToBitmapSource(colorImageFrame);
+            }
+        }
+    }
+}
